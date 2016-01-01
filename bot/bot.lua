c38ae267658e7258e0bd25d5a6d6f5dbd29c0c77
ArashInfestor <arashmardan.am@gmail.com>
2016-01-01 08:37:39 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {109722284},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
