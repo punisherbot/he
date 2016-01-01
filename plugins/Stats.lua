d1d6550246fb554877dd9c21583073c8a9a53d9a
ArashInfestor <arashmardan.am@gmail.com>
2016-01-01 08:36:29 -0800

    Update and rename stats.lua to Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -147,9 +147,9 @@ local function run(msg, matches)
       end
     end
 
-    if matches[2] == "bot" then
+    if matches[2] == "infernal" then
       if not is_sudo(msg) then
-        return "Bot stats requires privileged user"
+        return "infernal stats requires privileged user"
       else
         return bot_stats()
       end
@@ -170,12 +170,12 @@ return {
   usage = {
     "!stats: Returns a list of Username [telegram_id]: msg_num",
     "!stats chat <chat_id>: Show stats for chat_id",
-    "!stats bot: Shows bot stats (sudo users)"
+    "!stats infernal: Shows bot stats (sudo users)"
   },
   patterns = {
     "^!([Ss]tats)$",
     "^!([Ss]tats) (chat) (%d+)",
-    "^!([Ss]tats) (bot)"
+    "^!([Ss]tats) (infernal)"
     }, 
   run = run,
   pre_process = pre_process
