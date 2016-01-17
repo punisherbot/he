93c867a12d16e2409215346e236b9218eca42976
punisherbot <absam14339@yahoo.com>
2016-01-17 18:06:14 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {109722284},
+    sudo_users = {168398326},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
